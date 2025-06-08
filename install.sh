f4d02242a48df35a4ef062b0e9f0806d56f8a0fe
Iviv122 <ivan.skrinnik.nomia@gmail.com>
2025-06-08 22:28:33 +0200

    fix

--- install.sh
+++ install.sh
@@ -4,15 +4,16 @@ sudo pacman -S i3-wm polybar ttf-jetbrains-mono-nerd alacritty fish nitrogen pic
 echo "Installing starship"
 curl -sS https://starship.rs/install.sh | sh
 
-echo "Change default terminal"
-chsh -s /usr/bin/fish
-fish
-
-echo "Setuping shell"
-set -U fish_greeting ""
-starship preset nerd-font-symbols > ~/.config/starship.toml
-
 echo "Copying folders"
+
+fishDEST="$HOME/.config/fish"
+fishSOURCE=".config/fish"
+
+if [ ! -d "$i3DEST" ]; then
+  mkdir -p "$i3DEST"
+fi
+
+cp -r "$i3SOURCE/"* "$i3DEST"
 
 i3DEST="$HOME/.config/i3"
 i3SOURCE=".config/i3"
@@ -22,4 +23,12 @@ if [ ! -d "$i3DEST" ]; then
 fi
 
 cp -r "$i3SOURCE/"* "$i3DEST"
+
+echo "Change default terminal"
+chsh -s /usr/bin/fish
+fish
+
+echo "Setuping shell"
+set -U fish_greeting ""
+starship preset nerd-font-symbols > ~/.config/starship.toml
 
