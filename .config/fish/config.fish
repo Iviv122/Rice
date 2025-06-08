84eb9877cba2c0937a80c2d0bb9b6e3b8a675c68
Iviv122 <ivan.skrinnik.nomia@gmail.com>
2025-06-08 22:10:57 +0200

    fish and install test

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,6 +1,8 @@
 if status is-interactive
     # Commands to run in interactive sessions can go here
 	starship init fish | source
+
     alias l="ls"
     alias sl="ls"
+    alias neofetch="fastfetch"
 end
